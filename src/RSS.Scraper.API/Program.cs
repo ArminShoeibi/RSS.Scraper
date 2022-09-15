1b0f1dc950f179a8ec3c0d82bfd9f5c903750ef1
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-09-15 20:57:26 +0430

    WIP

--- src/RSS.Scraper.API/Program.cs
+++ src/RSS.Scraper.API/Program.cs
@@ -1,9 +1,13 @@
+using RSS.Scraper.API.BackgroundServices;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddHostedService<RssUpdaterBackgroundService>();
 var app = builder.Build();
 
 app.UseSwagger();
