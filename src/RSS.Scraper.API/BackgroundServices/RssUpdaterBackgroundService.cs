c27aace08a507c13534ba6addb6a7fca70b2e389
Armin Shoeibi <a.shoeibi@alibaba.ir>
2022-09-16 17:22:38 +0430

    WIP

--- src/RSS.Scraper.API/BackgroundServices/RssUpdaterBackgroundService.cs
+++ src/RSS.Scraper.API/BackgroundServices/RssUpdaterBackgroundService.cs
@@ -24,11 +24,3 @@ public class RssUpdaterBackgroundService : BackgroundService
         return Task.CompletedTask;
     }
 }
-
-public class RssFeed
-{
-    public int RssFeedId { get; set; }
-    public string Link { get; set; }
-    public string Title { get; set; }
-    public string Description { get; set; }
-}
---
+++ src/RSS.Scraper.API/Entities/RssFeed.cs
@@ -0,0 +1,9 @@
+﻿namespace RSS.Scraper.API.Entities;
+
+public class RssFeed
+{
+    public int RssFeedId { get; set; }
+    public string Link { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
